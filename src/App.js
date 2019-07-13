81f9b26ff03661218edf64982cd7bb17321d4dff
danielm2402 <damunoz216@unicauca.edu.co>
2019-07-13 08:06:09 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PageLanding from './componentes/paginas/PageLanding';
+
+function App() {
+  return (
+    <PageLanding/>
+  );
+}
+
+export default App;
---
+++ src/componentes/paginas/PageLanding.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import '../css/style.css'
+class PageLanding extends React.Component{
+    render(){
+        return(
+           <div className="container">
+               <div className="container__item landing-page-container">
+                    <div className="content_wrapper">
+                        <header className="header">
+                            <div className="menu-icon header__item">
+                                <span className="menu-icon__line"></span>
+                            </div>
+
+                            <h1 className="heading header__item">T R I P C O S T</h1>
+                            <ul className="social-container.header__item">
+                                <li className="social__icon social__icon--fb">
+                                    
+
+                                </li>
+                                <li className="social__icon social__icon--in">
+                                   
+                                    
+                                </li>
+                                <li className="social__icon social__icon--tw">
+                               
+                                    
+                                </li>
+                            </ul>
+                        </header>
+                        <div className="ellipse-container">
+                            <h2 className="greeting">EXPLORE</h2>
+                            <div className="ellipse ellipse__outer--thin">
+
+                                <div className="ellipse ellipse__orbit"></div>
+                            </div>
+                            <div className="ellipse ellipse__outer--thick"></div>
+                            <div className="btnStart">
+                            <a href="#">Start</a>
+
+                            </div>
+                            
+
+
+                        </div>
+                    </div>
+               </div>
+           </div>
+        );
+    }
+}
+export default PageLanding;
